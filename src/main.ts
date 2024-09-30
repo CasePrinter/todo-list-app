1f96d815cec27f969209004be8af78fe09fc9c71
luiscla1987 <luiscla1987@gmail.com>
2024-09-30 10:07:58 -0300

    fix(port): ajusta porta para 8080 deploy cloud run

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   // const app = await NestFactory.create(AppModule);
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
   
-  const port = process.env.PORT || 3000
+  const port = process.env.PORT || 8080
 
   app.setGlobalPrefix('api')
   
